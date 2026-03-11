36b5dcfcbc6be0919b16742ec5bf52010ee09077
Duong Van Ngoc <duongvanngoc150903@gmail.com>
2026-03-11 11:34:10 +0700

    fix: deploy vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,7 @@ import {Provider} from 'react-redux';
 import {store} from './app/store.js';
 import {ToastContainer} from 'react-toastify';
 
-
-//////////////////////////// // Entry point of the React application. It renders the App component wrapped with Redux Provider and ToastContainer for notifications.
-
+//////////////// ///// Entry Point of the Application ///// ////////////////
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store = {store}>
