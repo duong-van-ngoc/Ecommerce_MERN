57b011617c661921b0881c76d1990f53b7b9231b
Duong Van Ngoc <duongvanngoc150903@gmail.com>
2026-03-11 11:10:45 +0700

    fix import jsx extensiom n

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import {store} from './app/store.js';
 import {ToastContainer} from 'react-toastify';
 
 
-
+//////////////////////////// // Entry point of the React application. It renders the App component wrapped with Redux Provider and ToastContainer for notifications.
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
